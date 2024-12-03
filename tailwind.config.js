58c4d74b4310526601a082d0988f81617d168d73
saagor16 <saagor.16@gmail.com>
2024-12-03 17:00:29 +0600

    Updated Tailwind CSS configuration: added primary, secondary, and button colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'primary':'#f9f7f3',
+        'secondary':'#1b2629',
+        'btnColor':'#9c702a'
+      }
+    },
   },
   plugins: [],
 }
