495e55d324be942686fa6e906224f49b49c8290a
saagor16 <saagor.16@gmail.com>
2024-12-03 16:57:30 +0600

    Added menu to the header section

--- src/components/Header/DesktopNav.jsx
+++ src/components/Header/DesktopNav.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const DesktopNav = ({menuItems,Logo}) => {
     return (
         <div className='h-16 flex justify-between items-center px-6 lg:px-12'>
             <a href='/'>
             <img src={Logo} alt="logo" /></a>
+            <ul className='flex gap-7'>
+                {
+                    menuItems?.map((menu,index)=>(
+                        <li key={index}>
+                            <Link to='/' className='font-medium capitalize text-secondary'>{menu}</Link>
+                        </li>
+                    ))
+                }
+            </ul>
         </div>
     );
 };
