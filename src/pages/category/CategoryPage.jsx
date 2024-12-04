2311d9e1a59aa4c097fbac3e2ed92a593b53b7e0
saagor16 <saagor.16@gmail.com>
2024-12-04 18:21:23 +0600

    Designed the UI for the CategoryPage

--- src/pages/category/CategoryPage.jsx
+++ src/pages/category/CategoryPage.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
+import { useParams } from "react-router-dom";
+import CategoryWrapper from "./CategoryWrapper";
 
 const CategoryPage = () => {
-    return (
-        <div>
-            CategoryPage
-        </div>
-    );
+  const { category } = useParams();
+  return (
+    <div className="px-6 lg:px-12 py-20">
+      <h1 className="text-center text-3xl py-10 font-semibold sm:text-6xl sm:leading-relaxed capitalize">{category}</h1>
+      <CategoryWrapper></CategoryWrapper>
+    </div>
+  );
 };
 
 export default CategoryPage;
