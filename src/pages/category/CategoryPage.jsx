dcfc6050ffb160d71d383fb880d4d45bd3cdf1d3
saagor16 <saagor.16@gmail.com>
2024-12-11 19:10:10 +0600

    Added fetchCategoryData function to Category Page for data fetching

--- src/pages/category/CategoryPage.jsx
+++ src/pages/category/CategoryPage.jsx
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import CategoryWrapper from "./CategoryWrapper";
+import axios from "axios";
 
 const CategoryPage = () => {
   const { category } = useParams();
+  const [items,setItems]=useState([]);
+  const [loading,setLoading]=useState(false);
+  const [error,setError]=useState(null);
+
+
+  useEffect(()=>{
+    const fetchCategoryData = async ()=>{
+      setLoading(true);
+
+      try {
+        const response = await axios.get(`http://localhost:5000/api/categories/${category}`);
+        setItems(response.data)
+      } catch (error) {
+        setError(error.message || "Error Loading category")
+      }
+    }
+    fetchCategoryData()
+  },[category])
+
   return (
     <div className="px-6 lg:px-12 py-20">
       <h1 className="text-center text-3xl py-10 font-semibold sm:text-6xl sm:leading-relaxed capitalize">{category}</h1>
       <CategoryWrapper></CategoryWrapper>
+
+
+      <ul>
+        {
+          items && items?.map(item=>{
+            <li key={item._id}>{item.name}</li>
+          })
+        }
+      </ul>
+
+
     </div>
   );
 };
