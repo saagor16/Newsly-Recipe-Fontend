a49ce5d007bd32bbabd21f424eadc990e26673ae
saagor16 <saagor.16@gmail.com>
2024-12-14 00:53:39 +0600

    Completed the FeaturedSection with full functionality and design

--- src/pages/Home/FeaturedSection.jsx
+++ src/pages/Home/FeaturedSection.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import FeaturedImg from "../../assets/featured.webp"
+
 
 const FeaturedSection = () => {
     return (
         <div className='overflow-hidden flex md:flex-row flex-col justify-between items-center sm:my-20 my-4 md:gap-20 gap-12 px-5 lg:px-10'>
-            <div>
-                <img src="" alt="" />
+            <div className='relative'>
+                <div className='absolute top-4 left-5 bg-white text-secondary px-3 py-1 rounded-md uppercase tracking-wider'>
+                    Featured Recipe
+                </div>
+                <img src={FeaturedImg} alt="Featured Img" className='rounded-md'/>
             </div>
+
+            <div className='text-start sm:w-1/2'>
+                <h2 className='text-3xl font-semibold text-secondary sm:text-5xl sm:leading-relaxed'>Pineapple + Smoked Jackfruit Pizza</h2>
+                <p className='text-xl mt-4 text-[#5c5c5c]'>Preheat the oven to 200°C. Spread tomato sauce on pizza dough, add toppings like smoked jackfruit, pineapple, mozzarella, and bake for 15-20 mins until golden. Garnish with fresh basil and serve warm.</p>
+                <div className='lg:mt-0 lg:flex-shrink-0'>
+                    <div className='mt-12 inline-flex '>
+                    <button className='px-8 py-4 hover:bg-btnColor text-secondary hover:text-white w-full transition ease-in duration-200 text-center text-base font-semibold border border-[#9c703a] focus:outline-none rounded-lg'>
+                        View Recipe
+                    </button>
+                    </div>
+                </div>
+            </div>   
         </div>
     );
 };
