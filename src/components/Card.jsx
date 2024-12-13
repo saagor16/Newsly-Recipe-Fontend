27ea19d0cf532a62980d14368e31c49d970120fa
saagor16 <saagor.16@gmail.com>
2024-12-13 08:52:54 +0600

    Added item name to card with React Router Link for navigation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Card = ({item}) => {
     return (
         <div className='container mx-auto flex justify-center md:justify-center'>
             <div className='max-w-sm '>
-                <div>
-                    <img src={item?.thumbnail_image} alt="thumbnail_image" />
+                <div className='bg-white relative shadow-lg hover:shadow-xl transition duration-500 rounded-lg'>
+                    <img src={item?.thumbnail_image} alt="thumbnail_image" className='rounded-t-lg'/>
+                    <div className='py-6 px-5 rounded-lg bg-white'>
+                        <Link to={`/item/${item._id}`}>
+                        <h1 className='text-gray-700 font-bold text-2xl mb-8 hover:text-gray-900 hover:cursor-pointer'>{item?.name}</h1>
+                        </Link>
+                        
+                        
+                    </div>
                 </div>
             </div>
         </div>
