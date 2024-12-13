87b6cdd5783d88b8162bffe37b6f38e142d3f060
saagor16 <saagor.16@gmail.com>
2024-12-13 10:18:58 +0600

    Added go clock icon to card and fetched preparation time from database

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,86 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { GoClock } from "react-icons/go";
+import { Link } from "react-router-dom";
 
-const Card = ({item}) => {
-    return (
-        <div className='container mx-auto flex justify-center md:justify-center'>
-            <div className='max-w-sm '>
-                <div className='bg-white relative shadow-lg hover:shadow-xl transition duration-500 rounded-lg'>
-                    <img src={item?.thumbnail_image} alt="thumbnail_image" className='rounded-t-lg'/>
-                    <div className='py-6 px-5 rounded-lg bg-white'>
-                        <Link to={`/item/${item._id}`}>
-                        <h1 className='text-gray-700 font-bold text-2xl mb-8 hover:text-gray-900 hover:cursor-pointer'>{item?.name}</h1>
-                        </Link>
-                        
-                        
-                    </div>
-                </div>
+const Card = ({ item }) => {
+  const categoryStyles = {
+    Entrees: {
+      backgroundColor: "#f0f5c4",
+      color: "#59871f",
+    },
+    Breakfast: {
+      backgroundColor: "#efedfa",
+      color: "#3c3a8f",
+    },
+    Lunch: {
+      backgroundColor: "#e5f7f3",
+      color: "#1f8787",
+    },
+    Desserts: {
+      backgroundColor: "#e8f5fa",
+      color: "#397a9e",
+    },
+    Sides: {
+      backgroundColor: "#feefc9",
+      color: "#d16400",
+    },
+    Drinks: {
+      backgroundColor: "#ffeae3",
+      color: "#f0493e",
+    },
+    default: {
+      backgroundColor: "#fff",
+      color: "#000",
+    }
+  };
+
+  const getCategoryStyle =(category)=>{
+    return categoryStyles[category] || categoryStyles.default;
+  }
+  const categoryStyle =getCategoryStyle(item?.category);
+  console.log(item?.more);
+  
+
+  return (
+    <div className="container mx-auto flex justify-center md:justify-center">
+      <div className="max-w-sm ">
+        <div className="bg-white relative shadow-lg hover:shadow-xl transition duration-500 rounded-lg">
+          <img
+            src={item?.thumbnail_image}
+            alt="thumbnail_image"
+            className="rounded-t-lg"
+          />
+          <div className="py-6 px-5 rounded-lg bg-white">
+            <Link to={`/item/${item._id}`}>
+              <h1 className="text-gray-700 font-bold text-2xl mb-8 hover:text-gray-900 hover:cursor-pointer">
+                {item?.name}
+              </h1>
+            </Link>
+
+            {/** category and reading times */}
+
+            <div className="flex justify-between items-center flex-wrap">
+              <button
+                className={`mt-6 py-2 px-4 font-medium rounded-lg shadow-md hover:shadow-lg transition duration-300`}
+                style={{
+                    backgroundColor : categoryStyle.backgroundColor,
+                    color:categoryStyle.color
+                }}
+              >
+                {item?.category}
+              </button>
+
+              <div className="flex items-center py-2 mt-6">
+                <GoClock></GoClock>
+                <span className="ml-1">{item?.more?.[0]?.prep_time || "Cook time not available"}</span>
+
+              </div>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Card;
