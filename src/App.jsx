f2f3ed9567df06e668b9a138f4cfa81510033092
saagor16 <saagor.16@gmail.com>
2024-12-15 10:46:41 +0600

    Footer component created and added to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import { Outlet } from 'react-router-dom'
 import './App.css'
 import Header from './components/Header/Header'
+import Footer from './components/Footer'
 
 function App() {
 
@@ -9,6 +10,7 @@ function App() {
     <div className='max-w-screen-2xl mx-auto'>
       <Header></Header>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   )
 }
